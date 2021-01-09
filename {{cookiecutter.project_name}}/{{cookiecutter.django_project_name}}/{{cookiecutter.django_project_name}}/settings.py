7cfdebb3b85a07c3b0521a770f81f532fd35bb09
IgnisDa <ignisda2001@gmail.com>
2021-01-09 20:15:23 +0530

    fix({{cookiecutter.project_name}}/{{cookiecutter.django_project_name}}/{{cookiecutter.django_project_name}}/settings.py): add flake8 ignores

--- {{cookiecutter.project_name}}/{{cookiecutter.django_project_name}}/{{cookiecutter.django_project_name}}/settings.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.django_project_name}}/{{cookiecutter.django_project_name}}/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = '{{cookiecutter.django_project_name}}.wsgi.application'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa: E501
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
