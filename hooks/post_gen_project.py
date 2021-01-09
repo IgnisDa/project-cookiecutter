38fc44fa7391c4d103614a8b9d82d72741d24bbe
IgnisDa <ignisda2001@gmail.com>
2021-01-09 17:46:29 +0530

    fix(hooks/post_gen_project.py): add environement variable

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,9 @@
-from django import setup
+import os
+
+import django
 from django.core.management import call_command
 
-setup()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{cookiecutter.django_project_name}}.settings')
+
+django.setup()
 call_command('makemigrations', '{{cookiecutter.django_user_model_app}}')
