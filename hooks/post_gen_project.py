376e6667adffb7c5eb7e877087642ad925f3590d
IgnisDa <ignisda2001@gmail.com>
2021-01-09 17:59:12 +0530

    feat(django): add migration for accounts app, delete hooks

--- hooks/post_gen_project.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import django
-from django.core.management import call_command
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{cookiecutter.django_project_name}}.settings')
-print(os.system('ls -la'))
-django.setup()
-call_command('makemigrations', '{{cookiecutter.django_user_model_app}}')
