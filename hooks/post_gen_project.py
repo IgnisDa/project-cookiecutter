b30421ea3e02ed168bffcd6d8785ef65476800a3
IgnisDa <ignisda2001@gmail.com>
2021-01-09 17:50:17 +0530

    feat: stuf

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,6 +4,6 @@ import django
 from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{cookiecutter.django_project_name}}.settings')
-
+print(os.system('ls -la'))
 django.setup()
 call_command('makemigrations', '{{cookiecutter.django_user_model_app}}')
