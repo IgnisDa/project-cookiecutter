97ed9765a71507bbe1d4ea1779847bab092eb3b3
IgnisDa <ignisda2001@gmail.com>
2021-01-13 22:53:00 +0530

    style(backend): run formatter

--- django-nuxt-docusaurus/{{cookiecutter.project_name}}/{{cookiecutter.django_project_name}}/{{cookiecutter.django_user_model_app}}/models.py
+++ django-nuxt-docusaurus/{{cookiecutter.project_name}}/{{cookiecutter.django_project_name}}/{{cookiecutter.django_user_model_app}}/models.py
@@ -6,21 +6,16 @@ from . import managers
 
 
 class CustomUser(AbstractUser):
-    username = models.CharField(
-        max_length=100,
-        help_text=_('The username of the user.')
-    )
-    email = models.EmailField(
-        help_text=_('The email address of the user.'), unique=True
-    )
+    username = models.CharField(max_length=100, help_text=_("The username of the user."))
+    email = models.EmailField(help_text=_("The email address of the user."), unique=True)
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     objects = managers.CustomUserManager()
 
     class Meta:
-        ordering = ('id',)
+        ordering = ("id",)
 
     def __str__(self):
         return self.email
