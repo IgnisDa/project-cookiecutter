8e44d11411461ad502ff7330b2915b64a17126ab
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-17 20:43:02 +0530

    migrate to scss

--- django-nuxt-docusaurus/{{cookiecutter.project_name}}/{{cookiecutter.nuxt_project_name}}/nuxt.config.js
+++ django-nuxt-docusaurus/{{cookiecutter.project_name}}/{{cookiecutter.nuxt_project_name}}/nuxt.config.js
@@ -11,7 +11,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/global.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
